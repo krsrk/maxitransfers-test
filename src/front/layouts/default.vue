4ac092583ef0ddc90747a32b0bc5fc2eb54ff5d8
Chris Becerra <christian.becerra@qualtopequiposextendidos.com>
2023-11-09 12:50:31 -0600

    create app layout

---
+++ src/front/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app id="inspire">
+
+        <v-app-bar color="primary">
+          <v-app-bar-title>Admin Empleados</v-app-bar-title>
+        </v-app-bar>
+
+        <v-main>
+            <v-container>
+                <slot />
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
