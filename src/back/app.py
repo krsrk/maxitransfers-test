89c1a11e3ed067f6db5c5b983f96391126577e25
Chris Becerra <christian.becerra@qualtopequiposextendidos.com>
2023-11-08 18:25:09 -0600

    crud of beneficiaries

--- src/back/app.py
+++ src/back/app.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from repositories.EmployeeRepository import EmployeeRepository
+from repositories.BeneficiaryRepository import BeneficiaryRepository
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pymssql://sa:s8s!Np#m76LV#iN@db:1433/admin'
@@ -22,6 +23,19 @@ class Beneficiary(db.Model):
     nationality = db.Column(db.String)
     participation_percentage = db.Column(db.Integer)
     employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
+            "birth_date": self.birth_date,
+            "curp": self.curp,
+            "ssn": self.ssn,
+            "phone_number": self.phone_number,
+            "nationality": self.nationality,
+            "participation_percentage": self.participation_percentage
+        }
 
 
 class Employee(db.Model):
@@ -95,6 +109,43 @@ def update_employees():
 def delete_employees():
     data = request.get_json()
     result_set = EmployeeRepository(db, Employee).delete_employee(data['employee_id'])
+
+    if result_set['on_error']:
+        return jsonify({'message': result_set['error_message']}), 500
+
+    return jsonify({'message': result_set['message']}), 201
+
+
+@app.route('/api/beneficiaries/employee/<int:employee_id>')
+def get_beneficiaries(employee_id):
+    result_set = BeneficiaryRepository(db, Beneficiary).get_beneficiaries(employee_id)
+    return jsonify(result_set)
+
+
+@app.route('/api/beneficiaries/employee/<int:employee_id>', methods=['POST'])
+def create_employee_beneficiaries(employee_id):
+    result_set = BeneficiaryRepository(db, Beneficiary).create_beneficiary(employee_id, request.get_json())
+
+    if result_set['on_error']:
+        return jsonify({'message': result_set['error_message']}), 500
+
+    return jsonify({'message': result_set['message']}), 201
+
+
+@app.route('/api/beneficiaries', methods=['PUT'])
+def update_employee_beneficiaries():
+    result_set = BeneficiaryRepository(db, Beneficiary).update_beneficiary(request.get_json())
+
+    if result_set['on_error']:
+        return jsonify({'message': result_set['error_message']}), 500
+
+    return jsonify({'message': result_set['message']}), 201
+
+
+@app.route('/api/beneficiaries', methods=['DELETE'])
+def delete_employee_beneficiaries():
+    data = request.get_json()
+    result_set = BeneficiaryRepository(db, Beneficiary).delete_beneficiary(data['beneficiary_id'])
 
     if result_set['on_error']:
         return jsonify({'message': result_set['error_message']}), 500
--- src/back/repositories/BeneficiaryRepository.py
+++ src/back/repositories/BeneficiaryRepository.py
@@ -1,22 +1,22 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 
-class EmployeeRepository:
+class BeneficiaryRepository:
     def __init__(self, db_engine, model) -> None:
         self.db = db_engine
         self.model = model
         self.db_engine = create_engine('mssql+pymssql://sa:s8s!Np#m76LV#iN@db:1433/admin')
 
-    def get_employees(self):
-        query = self.model.query.all()
-        employees = [employee.to_dict() for employee in query]
+    def get_beneficiaries(self, employee_id):
+        query = self.model.query.filter_by(employee_id=employee_id).all()
+        beneficiaries = [beneficiary.to_dict() for beneficiary in query]
 
-        return employees
+        return beneficiaries
 
-    def get_employee_by_id(self):
+    def get_beneficiary_by_id(self):
         pass
 
-    def create_employee(self, data):
+    def create_beneficiary(self, employee_id, data):
         conn = self.db_engine.raw_connection()
         cursor = conn.cursor()
         result = {
@@ -27,20 +27,21 @@ class EmployeeRepository:
 
         try:
             sql = """
-                EXEC sp_create_employee %s, %s, %s, %s, %s, %s, %s, %s
+                EXEC sp_create_beneficiary %s, %s, %s, %s, %s, %s, %s, %s, %s
             """
             cursor.execute(sql, (
                 data['name'],
                 data['last_name'],
                 data['birth_date'],
-                data['employee_number'],
                 data['curp'],
                 data['ssn'],
                 data['phone_number'],
-                data['nationality']
+                data['nationality'],
+                data['participation_percentage'],
+                employee_id,
             ))
             conn.commit()
-            result['message'] = "Employee created successfully"
+            result['message'] = "Beneficiary created successfully"
             result['on_error'] = False
         except Exception as err:
             result['on_error'] = True
@@ -51,7 +52,7 @@ class EmployeeRepository:
 
         return result
 
-    def update_employee(self, data):
+    def update_beneficiary(self, data):
         conn = self.db_engine.raw_connection()
         cursor = conn.cursor()
         result = {
@@ -62,21 +63,21 @@ class EmployeeRepository:
 
         try:
             sql = """
-                EXEC sp_update_employee %s, %s, %s, %s, %s, %s, %s, %s, %s
+                EXEC sp_update_beneficiary %s, %s, %s, %s, %s, %s, %s, %s, %s
             """
             cursor.execute(sql, (
-                data['employee_id'],
+                data['beneficiary_id'],
                 data['name'],
                 data['last_name'],
                 data['birth_date'],
-                data['employee_number'],
                 data['curp'],
                 data['ssn'],
                 data['phone_number'],
-                data['nationality']
+                data['nationality'],
+                data['participation_percentage'],
             ))
             conn.commit()
-            result['message'] = "Employee updated successfully"
+            result['message'] = "Beneficiary updated successfully"
             result['on_error'] = False
         except Exception as err:
             result['on_error'] = True
@@ -87,7 +88,7 @@ class EmployeeRepository:
 
         return result
 
-    def delete_employee(self, employee_id):
+    def delete_beneficiary(self, beneficary_id):
         conn = self.db_engine.raw_connection()
         cursor = conn.cursor()
         result = {
@@ -98,11 +99,11 @@ class EmployeeRepository:
 
         try:
             sql = """
-                EXEC sp_delete_employee %s
+                EXEC sp_delete_beneficiary %s
             """
-            cursor.execute(sql, (employee_id))
+            cursor.execute(sql, (beneficary_id))
             conn.commit()
-            result['message'] = "Employee deleted successfully"
+            result['message'] = "Beneficiary deleted successfully"
             result['on_error'] = False
         except Exception as err:
             result['on_error'] = True
