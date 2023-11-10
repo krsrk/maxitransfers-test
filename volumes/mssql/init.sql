cdf74ced344ff9d4e57e6aa82e3f16943979dd6d
Chris Becerra <christian.becerra@qualtopequiposextendidos.com>
2023-11-10 16:04:24 -0600

    fix init.sql

--- volumes/mssql/init.sql
+++ volumes/mssql/init.sql
@@ -4,7 +4,13 @@ IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'admin')
     END;
 GO
 
-create table employee
+USE admin;
+GO
+
+DROP TABLE IF EXISTS dbo.employee;
+GO
+
+create table dbo.employee
 (
     id              int identity
         primary key,
@@ -19,7 +25,10 @@ create table employee
 )
 go
 
-create table beneficiary
+DROP TABLE IF EXISTS dbo.beneficiary;
+GO
+
+create table dbo.beneficiary
 (
     id                       int identity
         primary key,
@@ -32,12 +41,14 @@ create table beneficiary
     nationality              varchar(max),
     participation_percentage int,
     employee_id              int          not null
-        references employee
         constraint fk_beneficiary_employee
             references employee
             on delete cascade
 )
 go
+
+DROP PROCEDURE IF EXISTS dbo.sp_create_employee;
+GO
 
 CREATE PROCEDURE dbo.sp_create_employee
     @name VARCHAR(250),
@@ -56,6 +67,10 @@ BEGIN
 END;
 
 go
+
+DROP PROCEDURE IF EXISTS dbo.sp_update_employee;
+GO
+
 
 CREATE PROCEDURE dbo.sp_update_employee
     @employee_id int,
@@ -76,6 +91,10 @@ END;
 
 go
 
+DROP PROCEDURE IF EXISTS dbo.sp_delete_employee;
+GO
+
+
 CREATE PROCEDURE dbo.sp_delete_employee(@employee_id int) AS
 BEGIN
     DELETE FROM employee
@@ -84,6 +103,10 @@ BEGIN
 END;
 
 go
+
+DROP PROCEDURE IF EXISTS dbo.sp_create_beneficiary;
+GO
+
 
 CREATE PROCEDURE dbo.sp_create_beneficiary
     @name varchar (250),
@@ -103,6 +126,10 @@ END;
 
 go
 
+DROP PROCEDURE IF EXISTS dbo.sp_update_beneficiary;
+GO
+
+
 CREATE PROCEDURE dbo.sp_update_beneficiary
     @beneficiary_id int,
     @name varchar (250),
@@ -121,6 +148,11 @@ BEGIN
 END;
 go
 
+DROP PROCEDURE IF EXISTS dbo.sp_delete_beneficiary;
+GO
+
+
+
 CREATE PROCEDURE dbo.sp_delete_beneficiary(@beneficiary_id int) AS
 BEGIN
     DELETE FROM beneficiary
@@ -130,16 +162,13 @@ END;
 
 go
 
-USE admin;
-GO
+INSERT INTO dbo.employee (name, last_name, birth_date, employee_number, curp, ssn, phone_number, nationality) VALUES (N'Monkey', N'D. Luffy', N'1991-11-02', N'001', N'DLUM911102HASCPH14', N'51048001140', N'4491878584', N'Mexicana');
+INSERT INTO dbo.employee (name, last_name, birth_date, employee_number, curp, ssn, phone_number, nationality) VALUES (N'Zoro', N'Roronoa', N'1991-11-10', N'002', N'RORZ911102HASCPH15', N'51048001141', N'4491878685', N'Mexicana');
+INSERT INTO dbo.employee (name, last_name, birth_date, employee_number, curp, ssn, phone_number, nationality) VALUES (N'Usopp', N'SogeKing', N'1991-11-02', N'003', N'SOGU911102HASCPH16', N'51048001142', N'4491878786', N'Mexicana');
 
-INSERT INTO admin.dbo.employee (name, last_name, birth_date, employee_number, curp, ssn, phone_number, nationality) VALUES (N'Monkey', N'D. Luffy', N'1991-11-02', N'001', N'DLUM911102HASCPH14', N'51048001140', N'4491878584', N'Mexicana');
-INSERT INTO admin.dbo.employee (name, last_name, birth_date, employee_number, curp, ssn, phone_number, nationality) VALUES (N'Zoro', N'Roronoa', N'1991-11-10', N'002', N'RORZ911102HASCPH15', N'51048001141', N'4491878685', N'Mexicana');
-INSERT INTO admin.dbo.employee (name, last_name, birth_date, employee_number, curp, ssn, phone_number, nationality) VALUES (N'Usopp', N'SogeKing', N'1991-11-02', N'003', N'SOGU911102HASCPH16', N'51048001142', N'4491878786', N'Mexicana');
-
-INSERT INTO admin.dbo.beneficiary (name, last_name, birth_date, curp, ssn, phone_number, nationality, participation_percentage, employee_id) VALUES (N'Beneficiario 1', N'X', N'1991-12-01', N'DLUM911102HASCPH14', N'51048001140', N'4491878584', N'Mexicana', 10, 1);
-INSERT INTO admin.dbo.beneficiary (name, last_name, birth_date, curp, ssn, phone_number, nationality, participation_percentage, employee_id) VALUES (N'Beneficiario 2', N'X', N'1991-12-01', N'DLUM911102HASCPH14', N'51048001140', N'4491878584', N'Mexicana', 20, 1);
-INSERT INTO admin.dbo.beneficiary (name, last_name, birth_date, curp, ssn, phone_number, nationality, participation_percentage, employee_id) VALUES (N'Beneficiario 3', N'X', N'1991-12-01', N'DLUM911102HASCPH14', N'51048001140', N'4491878584', N'Mexicana', 10, 2);
-INSERT INTO admin.dbo.beneficiary (name, last_name, birth_date, curp, ssn, phone_number, nationality, participation_percentage, employee_id) VALUES (N'Beneficiario 4', N'X', N'1991-12-01', N'DLUM911102HASCPH14', N'51048001140', N'4491878584', N'Mexicana', 30, 2);
-INSERT INTO admin.dbo.beneficiary (name, last_name, birth_date, curp, ssn, phone_number, nationality, participation_percentage, employee_id) VALUES (N'Beneficiario 5', N'X', N'1991-12-01', N'DLUM911102HASCPH14', N'51048001140', N'4491878584', N'Mexicana', 10, 3);
-INSERT INTO admin.dbo.beneficiary (name, last_name, birth_date, curp, ssn, phone_number, nationality, participation_percentage, employee_id) VALUES (N'Beneficiario 6', N'X', N'1991-12-01', N'DLUM911102HASCPH14', N'51048001140', N'4491878584', N'Mexicana', 40, 3);
+INSERT INTO dbo.beneficiary (name, last_name, birth_date, curp, ssn, phone_number, nationality, participation_percentage, employee_id) VALUES (N'Beneficiario 1', N'X', N'1991-12-01', N'DLUM911102HASCPH14', N'51048001140', N'4491878584', N'Mexicana', 10, 1);
+INSERT INTO dbo.beneficiary (name, last_name, birth_date, curp, ssn, phone_number, nationality, participation_percentage, employee_id) VALUES (N'Beneficiario 2', N'X', N'1991-12-01', N'DLUM911102HASCPH14', N'51048001140', N'4491878584', N'Mexicana', 20, 1);
+INSERT INTO dbo.beneficiary (name, last_name, birth_date, curp, ssn, phone_number, nationality, participation_percentage, employee_id) VALUES (N'Beneficiario 3', N'X', N'1991-12-01', N'DLUM911102HASCPH14', N'51048001140', N'4491878584', N'Mexicana', 10, 2);
+INSERT INTO dbo.beneficiary (name, last_name, birth_date, curp, ssn, phone_number, nationality, participation_percentage, employee_id) VALUES (N'Beneficiario 4', N'X', N'1991-12-01', N'DLUM911102HASCPH14', N'51048001140', N'4491878584', N'Mexicana', 30, 2);
+INSERT INTO dbo.beneficiary (name, last_name, birth_date, curp, ssn, phone_number, nationality, participation_percentage, employee_id) VALUES (N'Beneficiario 5', N'X', N'1991-12-01', N'DLUM911102HASCPH14', N'51048001140', N'4491878584', N'Mexicana', 10, 3);
+INSERT INTO dbo.beneficiary (name, last_name, birth_date, curp, ssn, phone_number, nationality, participation_percentage, employee_id) VALUES (N'Beneficiario 6', N'X', N'1991-12-01', N'DLUM911102HASCPH14', N'51048001140', N'4491878584', N'Mexicana', 40, 3);
